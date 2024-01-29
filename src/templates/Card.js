3d6d9463b46a0b5e5f4295cab01436884c2c004b
Nani <lakshminarayanamalineni1@gmail.com>
2024-01-29 15:32:24 +0530

    link changes

--- src/templates/Card.js
+++ src/templates/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Card(props) {
   return (
     <div class="card" style={{width: "18rem"}}>
-        <img src={props.imageURL} class="card-img-top" alt="..."/>
+        <a href={props.Course_Link} class="card-link" target="_blank"><img src={props.imageURL} class="card-img-top" alt="..."/></a>
         <div class="card-body">
             <h4 class="card-title">{props.title} </h4><i style ={{fontSize :"13px"}}>{props.Type_of_University}</i>
             <p class="card-text">{props.degree} in {props.subject}</p>
